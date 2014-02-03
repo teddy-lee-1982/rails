c7e182638618f2355f075b9f661dbb769a289200
Lukasz Strzalkowski <lukaszstrzalkowski@squareup.com>
2014-02-04 00:39:06 +0100

    Run only mysql2 test on travis

--- ci/travis.rb
+++ ci/travis.rb
@@ -5,8 +5,6 @@ include FileUtils
 commands = [
   'mysql -e "create database activerecord_unittest;"',
   'mysql -e "create database activerecord_unittest2;"',
-  'psql  -c "create database activerecord_unittest;" -U postgres',
-  'psql  -c "create database activerecord_unittest2;" -U postgres'
 ]
 
 commands.each do |command|
